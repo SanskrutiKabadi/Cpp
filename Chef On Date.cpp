afd7f0ef0e7a71e79755088924c96573605f8660
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-05-07 07:15:58 +0530

    Chef On Date

---
+++ Chef On Date.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int X,Y;
+	    cin>>X>>Y;
+	    if(X>=Y)
+	        cout<<"YES"<<endl;
+	    else
+	        cout<<"NO"<<endl;
+	    
+	}
+	return 0;
+}
