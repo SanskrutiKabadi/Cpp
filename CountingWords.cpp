ac290045240b644c2f5c23a5f7f1744793610d2c
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-05-17 16:14:23 +0530

    CountingWords

---
+++ CountingWords.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int N,M;
+        cin>>N>>M;
+        cout<<(N*M)<<endl;
+        
+    }
+	return 0;
+}
