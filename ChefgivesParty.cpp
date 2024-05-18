d70131dd17a6176b7ef76f0315c0471a114e4ba2
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-05-18 18:18:07 +0530

    Chef gives Party

---
+++ ChefgivesParty.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int N,X,K;
+        cin>>N>>X>>K;
+        if(K>=(N*X))
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+        
+        
+        
+    }
+    
+	return 0;
+}
