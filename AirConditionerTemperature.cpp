567ef7e9a67c40afa4a3f62c75ea385ba8fe612c
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-05-21 14:48:10 +0530

    AirConditionerTemperature.cpp

---
+++ AirConditionerTemperature.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int A,B,C;
+	    cin>>A>>B>>C;
+	    if(B>=A and B>=C)
+	        cout<<"YES"<<endl;
+	    else
+	        cout<<"NO"<<endl;
+	    
+	    
+	    
+	    
+	    
+	}
+	return 0;
+}
