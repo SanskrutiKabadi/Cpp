5d6afd38f105e7abb6ea66a9065f3860dc0311fa
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-05-21 14:45:35 +0530

    GoodProgram.cpp

---
+++ GoodProgram.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int N;
+	    cin>>N;
+	    
+	    if((N%4)!=0)
+	        cout<<"NOT GOOD"<<endl;
+	    else
+	        cout<<" GOOD"<<endl;
+	    
+	    
+	}
+	return 0;
+}
